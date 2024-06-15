af3736a86cf0339150b198799ef223dd4bde45d9
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-06-15 15:06:04 -0600

    Agrega consulta simulada a API y mapeo de datos

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/domain/models/video_post_model.dart';
+import 'package:toktik/shared/data/local_videos.dart';
 
 class DiscoverProvider extends ChangeNotifier {
   bool initialLoading = true;
-  List<VideoPost> videoPosts = [];
+  List<VideoPost> videos = [];
 
   Future<void> loadNextPage() async {
-    // TODO
-
+    await Future.delayed(const Duration(seconds: 2));
+    List<VideoPost> newVideos = videoPosts.map(
+      (video) => VideoPostModel.fromJson(video).toVideoPostEntity()
+    ).toList();
+    videos.addAll(newVideos);
+  initialLoading = false;
     notifyListeners();
   }
 
